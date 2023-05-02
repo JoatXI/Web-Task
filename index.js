c5018c0994cfa7923f7ab6f3d0bbdc59cd43c92f
Michael Paterson <michaelpaterson194@gmail.com>
2023-05-02 08:14:53 +0100

    local server complete

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const app = express();
-const path = require('path');
+const PORT = 5000
 
 // Serve static files from the public directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname, + 'public/css'))
+app.use('/img', express.static(__dirname, + 'public/img'))
+app.use('/js', express.static(__dirname, + 'public/js'))
+
+app.set('views', './views');
+app.set('view engine', 'ejs')
 
 // Define a route handler for the default home page
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.render('index')
 });
 
 // Start the server on port 5000
-app.listen(5000, () => {
-  console.log('Server started on http://localhost:5000/');
-});
+app.listen(5000, () => console.log('Server started on http://localhost:5000/'));
